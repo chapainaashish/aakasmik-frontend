5388e35fa99979290c8437e647aa3b44673e5d0a
Aashish Chapain <ashishchapain86@gmail.com>
2024-09-07 21:32:12 +0545

    added new filters

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 import { VitePWA, VitePWAOptions } from 'vite-plugin-pwa';
 
-
-
-const manifestForPlugIn: Partial<VitePWAOptions> = { 
+const manifestForPlugIn: Partial<VitePWAOptions> = {
   registerType: 'prompt',
   includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'mask-icon.svg'],
   manifest: {
@@ -12,46 +10,47 @@ const manifestForPlugIn: Partial<VitePWAOptions> = {
     short_name: 'Aakasmik',
     description: 'Search and Find Emergency Contacts Near You',
     icons: [
-        {
-            src: 'public/pwa-64x64.png',
-            sizes: '64x64', 
-            type: 'image/png',
-            purpose:'favicon'
-        },
-        {
-            src: 'public/pwa-192x192.png',
-            sizes: '192x192',
-            type: 'image/png'
-        },
-        {
-            src: 'public/apple-touch-icon-180x180.png',
-            sizes: '180x180',
-            type: 'image/png',
-            purpose:'apple touch icon',
-        },
-        {
-            src: 'public/pwa-512x512.png',
-            sizes: '512x512',
-            type: 'image/png',
-            purpose: 'any'
-        },
-        {
-            src: 'public/maskable-icon-512x512.png',
-            sizes: '512x512',
-            type: 'image/png',
-            purpose: 'maskable'
-        }
+      {
+        src: '/pwa-64x64.png',
+        sizes: '64x64',
+        type: 'image/png',
+        purpose: 'favicon',
+      },
+      {
+        src: '/pwa-192x192.png',
+        sizes: '192x192',
+        type: 'image/png',
+      },
+      {
+        src: '/apple-touch-icon-180x180.png',
+        sizes: '180x180',
+        type: 'image/png',
+        purpose: 'apple touch icon',
+      },
+      {
+        src: '/pwa-512x512.png',
+        sizes: '512x512',
+        type: 'image/png',
+        purpose: 'any',
+      },
+      {
+        src: '/maskable-icon-512x512.png',
+        sizes: '512x512',
+        type: 'image/png',
+        purpose: 'maskable',
+      },
     ],
-    theme_color:'#171717',
-    background_color:'#f0e7db',
-    display:"standalone",
-    scope:'/',
-    start_url:"/",
-    orientation:'portrait'
-  }, 
-}
+    theme_color: '#171717',
+    background_color: '#f0e7db',
+    display: 'standalone',
+    scope: '/',
+    start_url: '/',
+    orientation: 'portrait',
+  },
+};
 
 export default defineConfig({
   plugins: [react(), VitePWA(manifestForPlugIn)],
-  envDir: '.', 
-})
+  envDir: '.',
+});
+ 
